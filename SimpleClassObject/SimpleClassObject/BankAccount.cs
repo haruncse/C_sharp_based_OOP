9bbc5ab1ab908f67c852ec3c19bb43b5e90749fa
Harun Rashid <harunorangebd.com>
2026-06-04 20:45:32 +0600

    PDF Added | Encapsulation

---
+++ SimpleClassObject/SimpleClassObject/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class BankAccount
+{
+    private decimal balance;
+    // Constructor
+
+    public BankAccount(decimal initialBalance) { 
+        if (initialBalance >= 0)
+            balance = initialBalance;
+        else
+            balance = 0;
+    }
+
+}
+
